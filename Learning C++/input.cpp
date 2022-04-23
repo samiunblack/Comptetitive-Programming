075cfa7da9e2a2c4a5bb1e2c7f25d30b90410ed8
Samiun Black <samiunblack@gmail.com>
2022-04-23 12:09:10 +0600

    learned about variables and input, output

---
+++ Learning C++/input.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int sum;
+
+    cout << "Enter a number: ";
+    cin >> a;
+
+    cout << "Enter another number: ";
+    cin >> b;
+
+    sum = a + b;
+    cout << "Sum: " << sum << endl;
+
+    return 0;
+}
---
+++ Learning C++/variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int tuna = 12;
+    cout << "tuna: " << tuna << endl;
+
+    int a = 4;
+    int b = 24;
+    int sum = a + b;
+
+    cout << "sum: " << sum << endl;
+
+    return 0;
+}
